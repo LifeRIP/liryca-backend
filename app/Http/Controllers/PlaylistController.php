11dfaf419f4c74504a18854c16879a6d8c69a15b
Zers04 <juandacata04@gmail.com>
2024-09-10 00:08:14 -0500

    Update playlist controller to follow documentation format
    In this commit, the playlist controller was modified to align with the format specified in the documentation. These changes ensure consistency with the project's guidelines and improve code maintainability.

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -3,252 +3,172 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 use App\Models\Playlist;
 use App\Models\User;
 use App\Models\SharedPlaylist;
 use App\Models\FollowedPlaylist;
 
+
 class PlaylistController extends Controller
 {
-    // añadir una nueva lista de reproducción
-    public function add_playlist(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
+        // Obtener todas las listas de reproducción publicas
+        $playlists = Playlist::where('privacy', 'public')->get();
 
-        try {
-            // Validar los campos requeridos
-            $validator = Validator::make($request->all(), [
-                'name' => 'required',
-                'user_id' => 'required',
-                'release_date' => 'required',
-                'description' => 'required',
-                'privacy' => 'required',
-                'image' => 'required',
-                'is_active' => 'required'
-            ]);
+        //si no hay listas de reproducción publicas
 
-            // Comprobar si la validación falla
-            if ($validator->fails()) {
-                return response()->json([
-                    'message' => 'Validation failed',
-                    'errors' => $validator->errors()
-                ], 400);
-            }
-
-            // Comprobar si el usuario existe 
-            $user = User::find($request->user_id);
-            if (!$user) {
-                return response()->json([
-                    'message' => 'User not found'
-                ], 404);
-            }
-
-            //Crear una nueva lista de reproducción
-            $playlist = new Playlist();
-            $playlist->name = $request->name;
-            $playlist->user_id = $request->user_id;
-            $playlist->release_date = $request->release_date;
-            $playlist->description = $request->description;
-            $playlist->privacy = $request->privacy;
-            $playlist->image = $request->image;
-            $playlist->is_active = $request->is_active;
-            $playlist->save();
-
-
+        if (!$playlists) {
             return response()->json([
-                'message' => 'Playlist created successfully',
-                'playlist' => $playlist
-            ], 201);
-        } catch (ValidationException $e) {
+                'message' => 'No playlists found'
+            ], 404);
+        }
+
+        return response()->json($playlists);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validar los campos requeridos
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'user_id' => 'required',
+            'release_date' => 'required',
+            'description' => 'required',
+            'privacy' => 'required',
+            'image' => 'required',
+            'is_active' => 'required'
+        ]);
+
+        // Comprobar si la validación falla
+        if ($validator->fails()) {
             return response()->json([
                 'message' => 'Validation failed',
-                'errors' => $e->errors()
+                'errors' => $validator->errors()
             ], 400);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'An error occurred',
-                'error' => $e->getMessage()
-            ], 500);
         }
+
+        // Comprobar si el usuario existe
+        $user = User::find($request->user_id);
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        // Crear una nueva lista de reproducción
+        $playlist = new Playlist();
+        $playlist->name = $request->name;
+        $playlist->user_id = $request->user_id;
+        $playlist->release_date = $request->release_date;
+        $playlist->description = $request->description;
+        $playlist->privacy = $request->privacy;
+        $playlist->image = $request->image;
+        $playlist->is_active = $request->is_active;
+        $playlist->save();
+
+        return response()->json([
+            'message' => 'Playlist created successfully',
+            'playlist' => $playlist
+        ], 201);
     }
 
-    // Obtener todas las listas de reproducción del usuario
-
-    public function get_playlists(Request $request)
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
     {
-        try {
-            // Validar los campos requeridos
-            $validator = Validator::make($request->all(), [
-                'user_id' => 'required'
-            ]);
-
-            // Comprobar si la validación falla
-            if ($validator->fails()) {
-                return response()->json([
-                    'message' => 'Validation failed',
-                    'errors' => $validator->errors()
-                ], 400);
-            }
-
-            // Obtener el ID del usuario
-            $user_id = $request->user_id;
-
-            // Comprobar si el usuario existe
-            $user = User::find($user_id);
-            if (!$user) {
-                return response()->json([
-                    'message' => 'User not found'
-                ], 404);
-            }
-
-            // Obtener todas las listas de reproducción del usuario
-            $playlists = Playlist::whereIn('id', function ($query) use ($user_id) {
-                $query->select('p.id')
-                    ->from('playlists as p')
-                    ->leftjoin('shared_playlists as sp', 'p.id', '=', 'sp.playlist_id')
-                    ->leftjoin('followed_playlists as fp', 'p.id', '=', 'fp.playlist_id')
-                    ->where('p.user_id', $user_id)
-                    ->orWhere('sp.user_id', $user_id)
-                    ->orWhere('fp.user_id', $user_id);
-            })->get();
-
+        // Obtener la lista de reproducción por nombre
+        $playlist = Playlist::where('name', $id)->first();
+        if (!$playlist) {
             return response()->json([
-                'message' => 'Playlists retrieved successfully',
-                'playlists' => $playlists
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'An error occurred',
-                'error' => $e->getMessage()
-            ], 500);
+                'message' => 'Playlist not found'
+            ], 404);
         }
+        return response()->json($playlist);
     }
 
-    // Modificar una lista de reproducción
-    public function update_playlist(Request $request)
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
     {
-        try {
-            // Validar los campos requeridos
-            $validator = Validator::make($request->all(), [
-                'playlistID' => 'required',
-                'user_id' => 'required',
-                'name' => 'required',
-                'release_date' => 'required',
-                'description' => 'required',
-                'privacy' => 'required',
-                'image' => 'required',
-                'is_active' => 'required'
-            ]);
+        // Buscar la lista de reproducción por ID
+        $playlist = Playlist::find($id);
 
-            // Comprobar si la validación falla
-            if ($validator->fails()) {
-                return response()->json([
-                    'message' => 'Validation failed',
-                    'errors' => $validator->errors()
-                ], 400);
-            }
-
-            // Comprobar si la lista de reproducción existe
-            $playlist = Playlist::find($request->playlistID);
-            if (!$playlist) {
-                return response()->json([
-                    'message' => 'Playlist not found'
-                ], 404);
-            }
-
-            // Comprobar si el usuario es el propietario de la lista de reproducción
-            if ($playlist->user_id !== $request->user_id) {
-                return response()->json([
-                    'message' => 'Unauthorized'
-                ], 401);
-            }
-
-            // Actualizar los campos de la lista de reproducción
-            if ($request->name) {
-                $playlist->name = $request->name;
-            }
-            if ($request->release_date) {
-                $playlist->release_date = $request->release_date;
-            }
-            if ($request->description) {
-                $playlist->description = $request->description;
-            }
-            if ($request->privacy) {
-                $playlist->privacy = $request->privacy;
-            }
-            if ($request->image) {
-                $playlist->image = $request->image;
-            }
-            if ($request->is_active) {
-                $playlist->is_active = $request->is_active;
-            }
-            $playlist->save();
-
-            // Retornar un mensaje de éxito y la lista de reproducción actualizada
+        // Si la lista de reproducción no existe
+        if (!$playlist) {
             return response()->json([
-                'message' => 'Playlist updated successfully',
-                'playlist' => $playlist
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'An error occurred',
-                'error' => $e->getMessage()
-            ], 500);
+                'message' => 'Playlist not found'
+            ], 404);
         }
+
+        // Comprobar si el usuario existe y es el propietario de la lista de reproducción
+        $user = User::find($request->user_id);
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        if ($playlist->user_id !== $request->user_id) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        // Actualizar la lista de reproducción
+        $playlist->update($request->all());
+
+        return response()->json([
+            'message' => 'Playlist updated successfully',
+            'playlist' => $playlist
+        ]);
     }
 
-    // Eliminar una lista de reproducción
-    public function delete_playlist(Request $request)
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, string $id)
     {
-        try {
-            // Validar los campos requeridos
-            $validator = Validator::make($request->all(), [
-                'playlistID' => 'required',
-                'user_id' => 'required'
-            ]);
+        // Buscar la lista de reproducción por ID
+        $playlist = Playlist::find($id);
 
-            // Comprobar si la validación falla
-            if ($validator->fails()) {
-                return response()->json([
-                    'message' => 'Validation failed',
-                    'errors' => $validator->errors()
-                ], 400);
-            }
-
-            // Comprobar si la lista de reproducción existe y está activa
-            $playlist = Playlist::where('id', $request->playlistID)
-                ->where('is_active', 1)
-                ->first();
-
-            if (!$playlist) {
-                return response()->json([
-                    'message' => 'Playlist not found'
-                ], 404);
-            }
-
-            // Comprobar si el usuario es el propietario de la lista de reproducción
-            if ($playlist->user_id !== $request->user_id) {
-                return response()->json([
-                    'message' => 'Unauthorized'
-                ], 401);
-            }
-
-            // Eliminar la lista de reproducción
-            $playlist->is_active = 0;
-
-            // Guardar los cambios
-            $playlist->save();
-
-            // Retornar un mensaje de éxito
+        // Si la lista de reproducción no existe
+        if (!$playlist) {
             return response()->json([
-                'message' => 'Playlist deleted successfully'
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'An error occurred',
-                'error' => $e->getMessage()
-            ], 500);
+                'message' => 'Playlist not found'
+            ], 404);
         }
+
+        // Comprobar si el usuario existe y es el propietario de la lista de reproducción
+        $user = User::find($request->user_id);
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        if ($playlist->user_id !== $request->user_id) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        // Eliminar la lista de reproducción
+        $playlist->delete();
+
+        return response()->json([
+            'message' => 'Playlist deleted successfully',
+            'data' => $playlist
+        ]);
     }
 }
--- routes/api/v1/playlist.php
+++ routes/api/v1/playlist.php
@@ -6,7 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 // Playlist //
 
-Route::post('/add', 'App\Http\Controllers\PlaylistController@add_playlist');
-Route::get('/get', 'App\Http\Controllers\PlaylistController@get_playlists');
-Route::put('/update', 'App\Http\Controllers\PlaylistController@update_playlist');
-Route::delete('/delete', 'App\Http\Controllers\PlaylistController@delete_playlist');
+Route::resource('/', PlaylistController::class)->parameters(['' => 'playlist']);
