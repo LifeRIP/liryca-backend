4ca8a5072903e91fd268c57ab9429162aa8e5135
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-10-05 19:33:02 -0500

    docs: Add comentary in LikeSeeder

--- database/seeders/LikeSeeder.php
+++ database/seeders/LikeSeeder.php
@@ -14,6 +14,7 @@ class LikeSeeder extends Seeder
      */
     public function run(): void
     {
+        // Crear un nuevo like
         Like::create([
             'user_id' => '264e1f6c-52ec-48ea-bfb1-13100f8b5cf3',
             'post_id' => Post::where('user_id', '8f6eea1e-9011-40ec-8ae4-19916127672b')->first()->id,
