0ac9f885abb6a63b8f8363de4c881727d42ef2ca
morbidslayer24 <tkgs2024@gmail.com>
2024-10-09 20:09:08 -0500

    Email recovery password
    Se agregó la funcionalidad para recuperar y cambiar contraseña mediante correo electrónico. Se implementaron las debidas rutas y controllers para el correcto funcionamiento. Cosas a tener en cuenta:
    
    -verificar que exista la tabla password_reset_token y hacer la migración si no existe
    -Al enviar el correo de recuperación al email de la cuenta, entrar al hipervínculo que genera el botón presente en el email, para que se abra una nueva pestaña en donde se tendra el token requerido en la url. Tomar este token y pegarlo en el body de la petición de passwrod reset en postman y colocar y confirmar la nueva contraseña.
    
    -verificar que en la tabla password_reset_token aparezca un token nuevo cada vez que envíe un correo de recuperación y que se elimine cuando cambie la contraseña exitosamente.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('passwordToken')->nullable();
             $table->string('google_id')->nullable();
             $table->string('facebook_id')->nullable();
             $table->enum('role', ['admin', 'user', 'artist'])->default('user');
