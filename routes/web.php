1eac9d12fcdb3b0a169afc6f5004e52f1d3a5a23
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-10-23 19:29:02 -0500

    refactor: Update OAuth provider redirect URLs to use environment variables
    - Update the redirect URLs for the OAuth providers (Google, Facebook, and GitHub) to use environment variables.
    - Add environment variables for the redirect URIs of Google, Facebook, and GitHub OAuth providers.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-//rutas para el inicio de sesión con Facebook
-Route::get('/login-facebook', [AuthController::class, 'redirectToFacebook']);
-Route::get('/facebook-callback', [AuthController::class, 'handleFacebookCallback']);
-
-//rutas para el inicio de sesión con Github
-Route::get('/login-github', [AuthController::class, 'redirectToGithub']);
-Route::get('/github-callback', [AuthController::class, 'handleGithubCallback']);
