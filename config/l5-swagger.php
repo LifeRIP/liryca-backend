3b4526abd20c82ab63cf35e4a7ec252674aadee2
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-11-14 15:46:32 -0500

    feat: specify HTTPS schemes in L5 Swagger configuration

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -6,6 +6,7 @@ return [
         'default' => [
             'api' => [
                 'title' => 'Liryca API',
+                'schemes' => ['https'],
             ],
 
             'routes' => [
