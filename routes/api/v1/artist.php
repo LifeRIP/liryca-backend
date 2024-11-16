4127995e7cdb9a3387327c0832ea52dde540f10e
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-11-16 18:22:05 -0500

    fix: update route parameter from 'user' to 'artist' in Artist routes

--- routes/api/v1/artist.php
+++ routes/api/v1/artist.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([
     'auth:sanctum'
 ])->group(function () {
-    Route::resource('/', UserController::class)->parameters(['' => 'user']);
+    Route::resource('/', UserController::class)->parameters(['' => 'artist']);
 });
 
 Route::get('/{artistId}/albums', [ArtistController::class, 'getAlbumsByArtist']);
