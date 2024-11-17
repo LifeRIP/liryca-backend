22e332b836a4cf6cb68420fa012ad193f9bca7d7
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-11-17 17:44:23 -0500

    feat: add endpoint to retrieve top songs by artist

--- routes/api/v1/song.php
+++ routes/api/v1/song.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('/', SongController::class)->parameters(['' => 'song']);
+    Route::get('/top-by-artist/{artistId}', [SongController::class, 'getTopSongsByArtist']);
 });
