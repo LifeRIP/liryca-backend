a678d050e0628619c2067410a28589147ef99f2d
Zers04 <juandacata04@gmail.com>
2024-11-17 22:08:36 -0500

    fix: Update follow controller and add check for individual artist following
    This commit includes a fix to the follow controller and adds a function to check if one artist follows another individually. These updates improve the functionality and accuracy of the follow system for artist relationships.

--- routes/api/v1/follow.php
+++ routes/api/v1/follow.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FollowsController;
 
+
 // Follow con middleware//
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/add_follow', [FollowsController::class, 'store']);
-    Route::get('/get_follows', [FollowsController::class, 'show']);
-    Route::delete('/delete_follow', [FollowsController::class, 'destroy']);
+    Route::resource('/', FollowsController::class)->parameters(['' => 'follow']);
+    Route::get('/followUnit/{id_following}', [FollowsController::class, 'followUnit']);
 });
