dcfb8417d7f67c24c0532fe22e6f764ac0e1e1db
LifeRIP <89425013+LifeRIP@users.noreply.github.com>
2024-11-20 07:39:42 -0500

    refactor: Artist authorization logic and comment route middleware for role-based access

--- routes/api/v1/artist.php
+++ routes/api/v1/artist.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\RoleEnum;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 
 // Artist //
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware([
+    'auth:sanctum',
+    //'role_or_permission' . RoleEnum::ARTIST
+])->group(function () {
     Route::resource('/', ArtistController::class)->parameters(['' => 'artist']);
 });
