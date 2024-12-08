4709adc7b08c578ad2cfb15838799365e141fca7
Zers04 <juandacata04@gmail.com>
2024-12-08 17:53:14 -0500

    refactor: Update post controller
    refactors the post controller to enhance code readability, optimize functionality, and align with best practices.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,10 +12,127 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(Request $request)
     {
         try {
-            $posts = Post::all();
+            // Obtener todos los posts en orden
+            $posts = Post::orderBy('created_at', 'desc')->get();
+
+            // Ootener el tipo de post 
+            foreach ($posts as $post) {
+                switch (true) {
+                    case $post->song_id:
+                        $post->type = 'song';
+                        break;
+                    case $post->album_id:
+                        $post->type = 'album';
+                        break;
+                    case $post->artist_id:
+                        $post->type = 'artist';
+                        break;
+                    case $post->playlist_id:
+                        $post->type = 'playlist';
+                        break;
+                }
+            }
+
+            // Obtener diversos datos de los posts 
+            $posts = $posts->map(function ($post) {
+                $post->user = $post->user;
+                switch ($post->type) {
+                    case 'song':
+                        $post->song = $post->song;
+                        break;
+                    case 'album':
+                        $post->album = $post->album;
+                        break;
+                    case 'artist':
+                        $post->artist = $post->artist;
+                        break;
+                    case 'playlist':
+                        $post->playlist = $post->playlist;
+                        break;
+                }
+                return $post;
+            });
+
+            // Organizar la información que se va a devolver
+            $posts = $posts->map(function ($post) {
+                switch ($post->type) {
+                    case 'song':
+                        return [
+                            'id' => $post->id,
+                            'user_id' => $post->user_id,
+                            'profile_picture' => $post->user->profile_picture,
+                            'username' => $post->user->username,
+                            'action_type' => $post->action_type,
+                            'content' => $post->content,
+                            'type' => $post->type,
+                            'song_id' => $post->song_id,
+                            'title' => $post->song->title,
+                            'duration' => $post->song->time,
+                            'genre' => $post->song->genre,
+                            'url_song' => $post->song->url_song,
+                            'album_id' => $post->song->album->id,
+                            'album_title' => $post->song->album->title,
+                            'icon' => $post->song->album->icon,
+                            'artist_id' => $post->song->artist->user_id,
+                            'artist_name' => $post->song->artist->user->username,
+                            'released_at' => $post->created_at,
+                        ];
+                    case 'album':
+                        return [
+                            'id' => $post->id,
+                            'user_id' => $post->user_id,
+                            'profile_picture' => $post->user->profile_picture,
+                            'username' => $post->user->username,
+                            'action_type' => $post->action_type,
+                            'content' => $post->content,
+                            'type' => $post->type,
+                            'album_id' => $post->album_id,
+                            'title' => $post->album->title,
+                            'description' => $post->album->description,
+                            'icon' => $post->album->icon,
+                            'artist_id' => $post->album->artist->user_id,
+                            'artist_name' => $post->album->artist->user->username,
+                            'released_at' => $post->created_at,
+                        ];
+                    case 'artist':
+                        return [
+                            'id' => $post->id,
+                            'user_id' => $post->user_id,
+                            'profile_picture' => $post->user->profile_picture,
+                            'username' => $post->user->username,
+                            'action_type' => $post->action_type,
+                            'content' => $post->content,
+                            'type' => $post->type,
+                            'artist_id' => $post->artist->user_id,
+                            'name' => $post->artist->user->username,
+                            'about' => $post->artist->about,
+                            'artist_profile_picture' => $post->artist->user->profile_picture,
+                            'artist_profile_banner' => $post->artist->user->profile_banner,
+                            'released_at' => $post->created_at,
+                        ];
+                    case 'playlist':
+                        return [
+                            'id' => $post->id,
+                            'user_id' => $post->user_id,
+                            'profile_picture' => $post->user->profile_picture,
+                            'username' => $post->user->username,
+                            'action_type' => $post->action_type,
+                            'content' => $post->content,
+                            'type' => $post->type,
+                            'playlist_id' => $post->playlist_id,
+                            'name' => $post->playlist->name,
+                            'owner_id' => $post->playlist->user_id,
+                            'owner_name' => $post->playlist->user->username,
+                            'description' => $post->playlist->description,
+                            'image' => $post->playlist->image,
+                            'released_at' => $post->created_at,
+                        ];
+                }
+            });
+
             return response()->json([
                 'success' => true,
                 'data' => $posts
@@ -74,59 +191,16 @@ class PostController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id): JsonResponse
-    {
-        try {
-            // Verificar si el post existe
-            $post = Post::find($id);
-            if (!$post) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Post not found'
-                ], 404);
-            }
-
-            // Mostrar el post
-            return response()->json([
-                'success' => true,
-                'data' => $post
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage()
-            ], 400);
-        }
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): JsonResponse
+    public function update(Request $request, string $id)
     {
         try {
             // Validar los campos requeridos
             $validator = Validator::make($request->all(), [
-                'user_id' => 'exists:users,id',
                 'content' => 'string',
-                'song_id' => 'exists:songs,id',
-                'album_id' => 'exists:albums,id',
-                'artist_id' => 'exists:artists,id',
-                'playlist_id' => 'exists:playlists,id',
                 'action_type' => 'in:shared,recommended,not_recommended',
-                'is_active' => 'boolean',
             ]);
-
-            // Verificar si el post existe
-            $post = Post::find($id);
-            if (!$post) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Post not found'
-                ], 404);
-            }
 
             // Comprobar si la validación falla
             if ($validator->fails()) {
@@ -134,6 +208,15 @@ class PostController extends Controller
                     'message' => 'Validation failed',
                     'errors' => $validator->errors()
                 ], 400);
+            }
+
+            // Verificar si el post existe
+            $post = Post::find($id);
+            if (!$post) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Post not found'
+                ], 404);
             }
 
             // Actualizar el post
@@ -153,7 +236,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): JsonResponse
+    public function destroy(Request $request, string $id): JsonResponse
     {
         try {
             // Verificar si el post existe
@@ -163,6 +246,14 @@ class PostController extends Controller
                     'success' => false,
                     'message' => 'Post not found'
                 ], 404);
+            }
+
+            // Verificar si el usuario autenticado es el propietario del post
+            if ($request->user()->id !== $post->user_id) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You are not authorized to delete this post'
+                ], 403);
             }
 
             // Eliminar el post
